418b0d07c70f2226b076fab81683510f0861edb9
Wolfy-J <wolfy.jd@gmail.com>
2019-10-07 13:58:35 +0300

    - test broadcast errors

--- redis_test.go
+++ redis_test.go
@@ -13,6 +13,26 @@ import (
 	"time"
 )
 
+func TestRedis_Error(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(env.ID, &env.Service{})
+	c.Register(rrhttp.ID, &rrhttp.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		http: `{
+			"address": ":6039",
+			"workers":{"command": "php tests/worker-ok.php", "pool.numWorkers": 1}
+		}`,
+		broadcast: `{"path":"/ws","redis":{"addr":"localhost:6372"}}`,
+	}))
+
+	assert.Error(t, c.Serve())
+}
+
 func TestRedis_Broadcast(t *testing.T) {
 	logger, _ := test.NewNullLogger()
 	logger.SetLevel(logrus.DebugLevel)
@@ -56,8 +76,13 @@ func TestRedis_Broadcast(t *testing.T) {
 		}
 	}()
 
-	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte(`{"cmd":"join", "args":["topic"]}`)))
 	assert.NoError(t, br.Broker().Broadcast(NewMessage("topic", "hello1"))) // must not be delivered
+
+	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte(`{"cmd":"join", "args":["topic"]}`)))
+	assert.Equal(t, `{"topic":"@join","payload":["topic"]}`, readStr(<-read))
+
+	// double join is OK
+	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte(`{"cmd":"join", "args":["topic"]}`)))
 	assert.Equal(t, `{"topic":"@join","payload":["topic"]}`, readStr(<-read))
 
 	assert.NoError(t, br.Broker().Broadcast(NewMessage("topic", "hello2")))
@@ -70,4 +95,57 @@ func TestRedis_Broadcast(t *testing.T) {
 
 	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte(`{"cmd":"join", "args":["topic"]}`)))
 	assert.Equal(t, `{"topic":"@join","payload":["topic"]}`, readStr(<-read))
+}
+
+func TestRedis_Broadcast_Error(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+	logger.SetLevel(logrus.DebugLevel)
+
+	c := service.NewContainer(logger)
+	c.Register(env.ID, &env.Service{})
+	c.Register(rrhttp.ID, &rrhttp.Service{})
+	c.Register(ID, &Service{})
+
+	assert.NoError(t, c.Init(&testCfg{
+		http: `{
+			"address": ":6039",
+			"workers":{"command": "php tests/worker-ok.php", "pool.numWorkers": 1}
+		}`,
+		broadcast: `{"path":"/ws","redis":{"addr":"localhost:6379"}}`,
+	}))
+
+	b, _ := c.Get(ID)
+	br := b.(*Service)
+
+	go func() { c.Serve() }()
+	time.Sleep(time.Millisecond * 100)
+	defer c.Stop()
+
+	u := url.URL{Scheme: "ws", Host: "localhost:6039", Path: "/ws"}
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	assert.NoError(t, err)
+	defer conn.Close()
+
+	read := make(chan interface{})
+
+	go func() {
+		defer close(read)
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			read <- message
+		}
+	}()
+
+	assert.NoError(t, conn.WriteMessage(websocket.TextMessage, []byte(`{"cmd":"join", "args":["topic"]}`)))
+	assert.Equal(t, `{"topic":"@join","payload":["topic"]}`, readStr(<-read))
+
+	assert.NoError(t, br.Broker().Broadcast(&Message{Topic: "topic", Payload: []byte("broken")}))
+	assert.Equal(t, ``, readStr(<-read))
+
+	assert.NoError(t, br.Broker().Broadcast(NewMessage("topic", "hello2")))
+	assert.Equal(t, `{"topic":"topic","payload":"hello2"}`, readStr(<-read))
 }
