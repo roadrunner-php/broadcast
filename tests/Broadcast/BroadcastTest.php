958fd4f608b98f405446e52ab6fc0c27ffae9226
Wolfy-J <wolfy.jd@gmail.com>
2019-10-07 16:02:44 +0300

    - testing ws-client in travis

--- tests/Broadcast/BroadcastTest.php
+++ tests/Broadcast/BroadcastTest.php
@@ -30,7 +30,7 @@ class BroadcastTest extends TestCase
         $rpc = new RPC(new SocketRelay("localhost", 6001));
         $br = new Broadcast($rpc);
 
-        $p = new Process("ws-client", dirname(__DIR__));
+        $p = new Process(dirname(__DIR__) . "/ws-client", dirname(__DIR__));
         $p->start();
 
         while (!file_exists(__DIR__ . '/../log.txt')) {
