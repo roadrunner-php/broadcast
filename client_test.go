76fee47b003ebfe5b1f6260fe07a69e539a679fd
Wolfy-J <wolfy.jd@gmail.com>
2019-11-10 17:11:49 +0300

    - improved testing

--- client_test.go
+++ client_test.go
@@ -17,14 +17,14 @@ func Test_Client_Topics(t *testing.T) {
 	assert.NoError(t, client.Subscribe("topic"))
 	assert.Equal(t, []string{"topic"}, client.Topics())
 
-	assert.NoError(t, client.Subscribe("topic"))
+	assert.NoError(t, br.broker.Subscribe(client.upstream, "topic"))
 	assert.Equal(t, []string{"topic"}, client.Topics())
 
 	assert.NoError(t, br.Broker().Publish(newMessage("topic", "hello1")))
 	assert.Equal(t, `hello1`, readStr(<-client.Channel()))
 
 	assert.NoError(t, client.Unsubscribe("topic"))
-	assert.NoError(t, client.Unsubscribe("topic"))
+	assert.NoError(t, br.broker.Unsubscribe(client.upstream, "topic"))
 
 	assert.Equal(t, []string{}, client.Topics())
 }
@@ -41,14 +41,14 @@ func Test_Client_Patterns(t *testing.T) {
 	assert.NoError(t, client.SubscribePattern("topic/*"))
 	assert.Equal(t, []string{"topic/*"}, client.Patterns())
 
-	assert.NoError(t, client.SubscribePattern("topic/*"))
+	assert.NoError(t, br.broker.SubscribePattern(client.upstream, "topic/*"))
 	assert.Equal(t, []string{"topic/*"}, client.Patterns())
 
 	assert.NoError(t, br.Broker().Publish(newMessage("topic/1", "hello1")))
 	assert.Equal(t, `hello1`, readStr(<-client.Channel()))
 
 	assert.NoError(t, client.UnsubscribePattern("topic/*"))
-	assert.NoError(t, client.UnsubscribePattern("topic/*"))
+	assert.NoError(t, br.broker.UnsubscribePattern(client.upstream, "topic/*"))
 
 	assert.Equal(t, []string{}, client.Patterns())
 }
